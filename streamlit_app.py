4bd5a5ade46fb6bf51050d21e7fbf72ce467c9be
Alfredo Cabanas <02cabanas@gmail.com>
2024-11-25 16:54:51 -0500

    added model and import to app file

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@ import streamlit as st
 import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
+import subprocess
 
 
 @st.cache_data
